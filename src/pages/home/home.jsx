81580779404c5bdb8958ab0d54e77ec2ee694504
SonOfBaphomet666 <142741515+SonOfBaphomet666@users.noreply.github.com>
2024-05-13 16:00:23 +0700

    footer

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,15 +9,33 @@ const Home = () => {
             <h1>Тут начинаются мысли!</h1>
           </div>
           <div className="hero__headingBox2">
-            <p>
-              Хочу поделиться с вами своим досугом.
-            </p>
+            <p>Хочу поделиться с вами своим досугом.</p>
           </div>
         </div>
       </section>
       <main>
         <Card />
       </main>
+      <footer>
+        <div className="footerBox">
+          <div className="box">
+            <div className="box__title">
+              <h5>Personally Newsletter</h5>
+              <h4>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+                placeat quidem dolor totam excepturi
+              </h4>
+              <div className="box__input">
+                <input placeholder="Email address" />
+                <button>Подписаться</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="info">
+          <p className="info__title">2024 Максим</p>
+        </div>
+      </footer>
     </>
   );
 };
