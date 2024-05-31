8d143923fc3085ab4f12ba8dd027fd748e70133d
SonOfBaphomet666 <142741515+SonOfBaphomet666@users.noreply.github.com>
2024-05-31 12:31:03 +0700

    useEffect
    useState

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,22 +1,55 @@
-let stat = function (response) {
-    if (response.status !== 200) {
-        return Promise.reject(new Error(response.statusText));
+function fetchData() {
+    let stat = function (response) {
+        if (response.status !== 200) {
+            return Promise.reject(new Error(response.statusText));
+        }
+        return Promise.resolve(response)
     }
-    return Promise.resolve(response)
-}
-let json = function (response) {
-    return response.json();
+    let json = function (response) {
+        return response.json();
+    }
+
+    fetch("https://run.mocky.io/v3/6e91b7a4-fa62-40f8-b5e6-b8d8b089033e", {
+        method: "post",
+        body: "test=1"
+    })
+        .then(stat)
+        .then(json)
+        .then(function (data) {
+            console.log("data", data);
+        })
+        .catch(function (error) {
+            console.log("error", error);
+        });
 }
 
-fetch("https://run.mocky.io/v3/6e91b7a4-fa62-40f8-b5e6-b8d8b089033e", {
-    method: "post",
-    body: "test=1"
-})
-    .then(stat)
-    .then(json)
-    .then(function (data) {
-        console.log("data", data);
-    })
-    .catch(function (error) {
-        console.log("error", error);
-    });
+export {fetchData};
+
+
+// ASYNC FUNCTION
+// async function fetchData() {
+//     const url = "https://run.mocky.io/v3/6e91b7a4-fa62-40f8-b5e6-b8d8b089033e";
+
+
+//     try {
+//         const response = await fetch(url, {
+//             method: "Post",
+//             body: "test=1"
+//         });
+
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+
+//         const data = await response.json();
+//         return data;
+
+//     } catch (err) {
+//         console.error("Error fetching or parsing data: ", err);
+//         throw err;
+//     }
+// }
+
+// export default fetchData;
+
+// END
--- src/components/hooks/useEffect.jsx
+++ src/components/hooks/useEffect.jsx
@@ -1,31 +1,82 @@
 import { useState, useEffect } from "react";
-// import {createCoonection} from '../../api/fetch';
+import {fetchData} from "../../api/fetch";
 
 const Fetch = () => {
-  const [serverUrl, setServerUrl] = useState([]);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("https://run.mocky.io/v3/6e91b7a4-fa62-40f8-b5e6-b8d8b089033e")
-      .then((response) => response.json())
-      .then((data) => setServerUrl(data))
-      .catch((error) => {
-        console.log("Error:", error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>Инфо</h1>
-      <ul>
-        {serverUrl.map((item, index) => {console.log(index); (
-          <li key={index[0]}>
-            <p>{item.date}</p>
-            <p>{item.imgUrl}</p>
-          </li>
-        )})}
-      </ul>
-    </div>
-  );
-};
+    fetchData()
+    
+    // let ignore = false;
+    // setData(null);
+    // fetchData
+  })
+}
 
 export default Fetch;
+
+
+// ASYNC FUNCTION
+// const Fetch = () => {
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//      async function loadData() {
+//       const result = await fetchData();
+//       setData(result);
+//     }
+
+//     loadData().catch((error) => {
+//       console.error("An error occured while loading the data: ", error);
+//     });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Инфо</h1>
+//       <ul>
+//               {data.map((item, index) => { (
+//                 <li key={index}>
+//                   <p>{item.date}</p>
+//                   <p>{item.imgUrl}</p>
+//                 </li>
+//               )})}
+//             </ul>
+//     </div>
+//   );
+// };
+
+// export default Fetch;
+
+// END
+
+// ALL TOGEHTER
+// const Fetch = () => {
+//   const [serverUrl, setServerUrl] = useState([]);
+
+//   useEffect(() => {
+//     fetch("https://run.mocky.io/v3/6e91b7a4-fa62-40f8-b5e6-b8d8b089033e")
+//       .then((response) => response.json())
+//       .then((data) => setServerUrl(data))
+//       .catch((error) => {
+//         console.log("Error:", error);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Инфо</h1>
+//       <ul>
+//         {serverUrl.map((item, index) => {console.log(index); (
+//           <li key={index[0]}>
+//             <p>{item.date}</p>
+//             <p>{item.imgUrl}</p>
+//           </li>
+//         )})}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Fetch;
+// END
